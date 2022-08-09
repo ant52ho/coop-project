1135c6fdc7b7287ed40ec899ed05796b5f0c2057
ant52ho <87661562+ant52ho@users.noreply.github.com>
2022-08-09 14:12:28 -0400

    repulling

--- hardware/scripts/IBSSsetup.py
+++ hardware/scripts/IBSSsetup.py
@@ -6,3 +6,7 @@ os.system('sudo ifconfig wlan0 down')
 os.system('sudo iwconfig wlan0 mode ad-hoc')
 os.system("sudo iwconfig wlan0 essid 'AHTest'")
 os.system('sudo ifconfig wlan0 10.0.1.1')
+
+
+#disconnect:
+os.system('sudo iwconfig wlan0 mode auto')
